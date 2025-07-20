f65b341abcc4c088e5683a268a032b8e80cb42cd
Markus Heimerl <markusheimerl@hotmail.com>
2025-07-20 18:35:54 +0200

    Remove load_data function prototype from header and implementation to streamline data handling

--- data.h
+++ data.h
@@ -6,10 +6,9 @@
 #include <string.h>
 #include <math.h>
 
-// Function prototypes - all handle sequence data for SSM
+// Function prototypes
 void generate_synthetic_data(float** X, float** y, int num_sequences, int seq_len, int input_dim, int output_dim, 
                            float input_min, float input_max);
 void save_data(float* X, float* y, int num_sequences, int seq_len, int input_dim, int output_dim, const char* filename);
-void load_data(const char* filename, float** X, float** y, int* num_sequences, int* seq_len, int input_dim, int output_dim);
 
 #endif
